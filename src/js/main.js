8cdb340c0b1dc0a429a51543f616b9320275801b
VolodymyrBuzinov <64635201+VolodymyrBuzinov@users.noreply.github.com>
2021-01-25 00:35:23 +0200

    add main files

--- src/js/main.js
+++
@@ -1,9 +0,0 @@
-const refs = {
-    header: document.querySelector('.header'), 
-}
-
-refs.header.addEventListener('click', evt => {
-    if (evt.target.tagName === 'A') {
-        evt.preventDefault();
-    }
-})
